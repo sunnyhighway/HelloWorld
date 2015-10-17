3a723d89d34d14023cb8aa3ff67018329ded65b8
bart.hijwegen <bart.hijwegen@regas.nl>
2015-10-17 12:11:20 +0200

    Buggy implementation

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace HelloWorld
 {
@@ -7,16 +8,42 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
             bool option = true;
 
             if (option == true)
             {
-                // TODO: Get user culture from user state
-                CultureInfo userCulture = new CultureInfo("Fr-fr", true);
+                RegasCultureInfo userCulture = new RegasCultureInfo(args[1]);
                 Console.WriteLine(string.Format(userCulture, "hello {0}.", args[0]));
+            }
+        }
 
-                Console.WriteLine("Press ant key to continue.");
-                Console.ReadKey();
+        class RegasCultureInfo :CultureInfo
+        {
+            
+            public RegasCultureInfo() : base("En-en") { }
+
+            public RegasCultureInfo(string name):base(GetUserCulture(name)) {}
+
+            private static string GetUserCulture(string language)
+            {
+                Dictionary<string, string> UserCulture = new Dictionary<string, string>(4);
+                UserCulture.Add("frans", "Fr-fr");
+                UserCulture.Add("duits", "De-de");
+                UserCulture.Add("nederlands", "Nl-nl");
+                UserCulture.Add("engels", "Fr-fr");
+
+                string culturename;
+                if (!UserCulture.TryGetValue(language, out culturename))
+                {
+                    throw new CultureNotFoundException(string.Format("User speaks {0}, we cant cope with that.", language));
+                }
+                else
+                {
+                    culturename = "EN-en";
+                }
+
+                return culturename;
             }
         }
     }
