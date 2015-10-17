8297e408623d01ab3847707b6541587bd3f9d986
bart.hijwegen <bart.hijwegen@regas.nl>
2015-10-17 10:33:56 +0200

    Fixed User can't read message bug.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,8 +11,12 @@ namespace HelloWorld
 
             if (option == true)
             {
+                // TODO: Get user culture from user state
                 CultureInfo userCulture = new CultureInfo("Fr-fr", true);
                 Console.WriteLine(string.Format(userCulture, "hello {0}.", args[0]));
+
+                Console.WriteLine("Press ant key to continue.");
+                Console.ReadKey();
             }
         }
     }
