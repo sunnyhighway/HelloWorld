75238236ce8f87639f0275403e77bb5610399c3d
bart.hijwegen <bart.hijwegen@regas.nl>
2015-10-18 07:34:34 +0200

    Implemented proper unknown language handling

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,25 +8,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            RegasCultureInfo userCulture = new RegasCultureInfo(args[1]);
+            Console.WriteLine(string.Format(userCulture, "hello {0}.", args[0]));
 
-            bool option = true;
-
-            if (option == true)
-            {
-                RegasCultureInfo userCulture = new RegasCultureInfo(args[1]);
-                Console.WriteLine(string.Format(userCulture, "hello {0}.", args[0]));
-
-                Console.WriteLine("press any key to continue.");
-                Console.ReadKey();
-            }
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
         }
 
         class RegasCultureInfo :CultureInfo
         {
 
+            enum WhenLanguageIsUnknown
+            {
+                ThrowException,
+                SetDefaultLanguage
+            }
+
             public RegasCultureInfo(string language) :base(GetUserCulture(language),true) {}
 
             private static string GetUserCulture(string language)
+            {
+                return GetUserCulture(language, WhenLanguageIsUnknown.ThrowException);
+            }
+
+            private static string GetUserCulture(string language, WhenLanguageIsUnknown whenLanguageIsUnknown)
             {
                 Dictionary<string, string> UserCulture = new Dictionary<string, string>(4);
                 UserCulture.Add("frans", "Fr-fr");
@@ -37,9 +42,19 @@ namespace HelloWorld
                 string culturename;
                 if (!UserCulture.TryGetValue(language, out culturename))
                 {
-                    throw new CultureNotFoundException(string.Format("User speaks {0}, we cant cope with that.", language));
-                    // Alternative: Use a failover language, for example: english
-                    //culturename = "En-en";
+                    switch (whenLanguageIsUnknown)
+                    {
+                        case WhenLanguageIsUnknown.ThrowException:
+                            throw new CultureNotFoundException(string.Format(CultureInfo.InvariantCulture ,"User speaks {0}, we cant cope with that.", language));
+
+                        case WhenLanguageIsUnknown.SetDefaultLanguage:
+                            culturename = "En-en";
+                            break;
+
+                        default:
+                            throw new NotImplementedException(string.Format(CultureInfo.InvariantCulture, "Undefined value for enum 'WhenLanguageIsUnknown': {0}.", whenLanguageIsUnknown.ToString()));
+
+                    }
                 }
 
                 return culturename;
